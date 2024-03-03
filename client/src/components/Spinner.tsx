18a76f17f7835be915fac913c01b380dbccf1a6c
Moriarity-2k <professormoriarity2000@gmail.com>
2024-03-03 07:58:09 +0530

    added error boundary client

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -40,7 +40,7 @@ export function SpiralSpinner({ color, sz }: IClipLoader) {
 					color={color}
 					loading={true}
 					cssOverride={override}
-					size={300}
+					// size={300}
 					aria-label="Loading Spinner"
 					data-testid="loader"
 					className="z-10 absolute top-0 left-0 flex-center h-screen w-full backdrop:blur-sm"
