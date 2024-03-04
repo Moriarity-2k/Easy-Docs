713c4364bf46b71c4276e59e02df56fbf3359b8c
Moriarity-2k <professormoriarity2000@gmail.com>
2024-03-04 07:18:08 +0530

    cors config server

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,17 +12,27 @@ import userRouter from "./routes/userRouter";
 import documentRouter from "./routes/documentRouter";
 import cookieParser from "cookie-parser";
 import ExpressMongoSanitize from "express-mongo-sanitize";
-import morgan from "morgan";
+// import morgan from "morgan";
 import compression from "compression";
 // import xss from "xss-clean";
 import AppError from "./utils/appError";
 import globalErrorHandler from "./controllers/errorController";
+import path from "path";
 
 const app = express();
 
 app.use(compression());
 
-
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Authorization, Content-Type"
+	);
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	next();
+});
 
 app.use(
 	helmet({
@@ -42,23 +52,23 @@ app.use(
 	})
 );
 
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
-}
+// if (process.env.NODE_ENV === "development") {
+// 	app.use(morgan("dev"));
+// }
 
 app.use(nocache());
 
-app.use(
-	cors({
-		// origin: process.env.CLIENT_ORIGIN_URL,
-		// origin: '*',
-        origin : ['*'],
-		methods: ["GET", "POST", "DELETE", "PUT"],
-		allowedHeaders: ["Authorization", "Content-Type"],
-		maxAge: 86400,
-		credentials: true,
-	})
-);
+// app.use(
+// 	cors({
+// 		// origin: process.env.CLIENT_ORIGIN_URL,
+// 		// origin: '*',
+//         origin : ['*'],
+// 		methods: ["GET", "POST", "DELETE", "PUT"],
+// 		allowedHeaders: ["Authorization", "Content-Type"],
+// 		maxAge: 86400,
+// 		credentials: true,
+// 	})
+// );
 
 app.use(ExpressMongoSanitize());
 // app.use(xss());
@@ -67,8 +77,17 @@ app.use(express.json());
 
 // routes ->
 
+app.set("view engine", "ejs");
+app.engine("ejs", require("ejs").__express);
+app.set("views", path.join(__dirname, "views"));
+
 app.use("/api/v1/gdocs", userRouter);
 app.use("/api/v1/gdocs", documentRouter);
+
+// views probably
+app.get("/", (req, res) => {
+	res.render("index");
+});
 
 app.all("*", (req, res, next) => {
 	next(new AppError(`Can't find ${req.url} on this server!`, 404));
